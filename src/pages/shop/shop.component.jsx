13052cd2ea48db4b5b7e0b25d56c9296c46542c5
chamo1116 <joan.chamorro@mercadolibre.com.co>
2021-09-12 18:25:46 -0500

    added shop data to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,23 @@
-import {Component} from 'react';
-import SHOP_DATA from './shop.data';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCollections } from '../../redux/shop/shop.selector';
+
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections : SHOP_DATA
+const ShopPage = ({ collections }) => (
+    <div className="shop-page">
+        {
+            collections.map(({ id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
+            ))
         }
-    }
+    </div>
+);
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-                    ))
-                }
-            </div>
-        )
-    }
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+});
 
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
 

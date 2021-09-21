4bba1e458831fb9d370918527929542b43edfccf
chamo1116 <joan.chamorro@mercadolibre.com.co>
2021-09-21 18:11:17 -0500

    add async redux and container pattern

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,31 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+import { collection, onSnapshot } from 'firebase/firestore';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTION_START
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+    type: ShopActionTypes.FETCH_COLLECTION_SUCCESS,
     payload: collectionsMap
+});
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTION_FAILURE,
+    payload: errorMessage
 })
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = collection(firestore, 'collections');
+        dispatch(fetchCollectionsStart());
+
+        onSnapshot(collectionRef, async snapshot => {
+            const collectionMap = await convertCollectionsSnapshotToMap(collectionRef, snapshot);
+            dispatch(fetchCollectionsSuccess(collectionMap));
+        }, error => dispatch(fetchCollectionsFailure(error.message)))
+    }
+}
